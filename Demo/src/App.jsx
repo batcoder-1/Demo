366f0e27b8bebe03abb9ccf886fac2eff066fc71
Naman Dadhich <ndmajor2005@gmail.com>
2025-10-03 01:49:51 +0530

    fix: correct import path for AuthPage component

--- Demo/src/App.jsx
+++ Demo/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import AuthPage from './components/Auth'
 import { Outlet } from 'react-router'
 import Header from './components/Header'
 import Footer from './components/Footer'
